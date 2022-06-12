8b5bbb4f6ea29ccb16883a9d528030d388dff38e
VoVanThanh <thanhvo618@gmail.com>
2022-06-12 20:32:19 +0700

    optimize performance header

--- F8/tiktok-ui/src/layouts/components/Search/Search.js
+++ F8/tiktok-ui/src/layouts/components/Search/Search.js
@@ -16,22 +16,22 @@ const cx = classNames.bind(styles);
 function Search() {
     const [searchValue, setSearchValue] = useState('');
     const [searchResult, setSearchResult] = useState([]);
-    const [showResult, setShowResult] = useState(true);
+    const [showResult, setShowResult] = useState(false);
     const [loading, setLoading] = useState(false);
 
     const inputRef = useRef();
 
-    const debounced = useDebounce(searchValue, 500);
+    const debouncedValue = useDebounce(searchValue, 500);
 
     useEffect(() => {
-        if (!debounced.trim()) {
+        if (!debouncedValue.trim()) {
             setSearchResult([]);
             return;
         }
         (async () => {
             try {
                 setLoading(true);
-                const result = await searchService.search(debounced);
+                const result = await searchService.search(debouncedValue);
                 setSearchResult(result);
                 setLoading(false);
             } catch (error) {
@@ -39,7 +39,7 @@ function Search() {
                 console.log(error);
             }
         })();
-    }, [debounced]);
+    }, [debouncedValue]);
 
     const handleClear = () => {
         setSearchValue('');

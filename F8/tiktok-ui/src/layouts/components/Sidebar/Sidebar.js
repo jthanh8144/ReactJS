c63dd006b5150a3af2efa78b7b18f85f0f9c4844
VoVanThanh <thanhvo618@gmail.com>
2022-06-16 22:33:51 +0700

    UI sidebar #1

---
+++ F8/tiktok-ui/src/layouts/components/Sidebar/Sidebar.js
@@ -0,0 +1,37 @@
+import classNames from 'classnames/bind';
+
+import config from '~/config';
+import Menu, { MenuItem } from './Menu';
+import { HomeIcon, UserGrouphIcon, LiveIcon, HomeActiveIcon, UserGrouphActiveIcon, LiveActiveIcon } from '~/components/Icons';
+import styles from './Sidebar.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Sidebar() {
+    return (
+        <aside className={cx('wrapper')}>
+            <Menu>
+                <MenuItem
+                    title="For you"
+                    to={config.routes.home}
+                    icon={<HomeIcon />}
+                    activeIcon={<HomeActiveIcon />}
+                />
+                <MenuItem
+                    title="Following"
+                    to={config.routes.following}
+                    icon={<UserGrouphIcon />}
+                    activeIcon={<UserGrouphActiveIcon />}
+                />
+                <MenuItem
+                    title="LIVE"
+                    to={config.routes.live}
+                    icon={<LiveIcon />}
+                    activeIcon={<LiveActiveIcon />}
+                />
+            </Menu>
+        </aside>
+    );
+}
+
+export default Sidebar;
